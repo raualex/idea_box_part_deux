f8a1776e354492d4b625b92c2e6312c5dd2f30cf
Alexander Rau <alexanderrau86@yahoo.com>
2018-09-26 14:52:58 -0600

    Start building idea box

---
+++ src/Header.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+class Header extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>idea<span className="box-title">box</span></h1>
+      </div>
+    )
+  }
+
+
+
+}
+
+export default Header;
