4566311942ebad70621d49386f33932d421616c9
Alexander Rau <alexanderrau86@yahoo.com>
2018-09-29 11:56:16 -0600

    Start adding functionality to save ideas to local storage

--- src/Header.js
+++ src/Header.js
@@ -5,6 +5,28 @@ import './CSS/Header.css'
 class Header extends Component {
   constructor() {
     super();
+
+    this.state = {
+      ideas: {}
+    }
+
+    this.handleClick = this.handleClick.bind(this)
+    this.clearInputs = this.clearInputs.bind(this)
+  }
+
+  handleClick(event) {
+    event.preventDefault()
+    let ideaTitle = document.querySelector('.input-title').value
+    let ideaBody = document.querySelector('.input-body').value
+    this.setState({
+      ideas: `{${ideaTitle}: ${ideaBody}}`
+    })
+    this.clearInputs()
+  }
+
+  clearInputs() {
+    document.querySelector('.input-title').value = ''
+    document.querySelector('.input-body').value = ''    
   }
 
   render() {
@@ -12,9 +34,9 @@ class Header extends Component {
       <div className="header">
         <h1 className="title-bar">idea<span className="box-title">box</span></h1>
         <form className="input-form">
-          <input type="text" />
-          <input type="text" />
-          <button>Submit</button>
+          <input type="text" className="input-title" />
+          <input type="text" className="input-body" />
+          <button className="input-button" onClick={this.handleClick}>Submit</button>
         </form>
       </div>
     )
